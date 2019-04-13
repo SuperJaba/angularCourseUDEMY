46b738bcc0abe04ed3f9eaa0d48d90068f6113b1
Karambix\Karambii <pichow@wp.pl>
2019-04-13 16:00:49 +0200

    new warrning-alert component, nested warningAlert component to servers template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarrningAlertComponent } from './warrning-alert/warrning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarrningAlertComponent
   ],
   imports: [
     BrowserModule
